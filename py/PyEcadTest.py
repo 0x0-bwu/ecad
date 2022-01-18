9fc14b7e2455a393e90809139df88a91c3995041
Draaaaaaven <350240702@qq.com>
2022-01-18 18:15:58 +0800

    update

--- py/PyEcadTest.py
+++ py/PyEcadTest.py
@@ -27,13 +27,21 @@ assert(database == None)
 #save database
 database = mgr.create_database(db_name)
 txt_file = current_dir + '/' + db_name + '.txt'
+if os.path.isfile(txt_file) :
+    os.remove(txt_file)
 assert(mgr.save_database(database, txt_file))
+assert(os.path.isfile(txt_file) == True)
 xml_file = current_dir + '/' + db_name + '.xml'
+if os.path.isfile(xml_file) :
+    os.remove(xml_file)
 assert(mgr.save_database(database, xml_file, libEcad.EArchiveFormat.XML))
+assert(os.path.isfile(xml_file) == True)
 
 #load database
 assert(mgr.load_database(database, txt_file))
 assert(mgr.load_database(database, xml_file, libEcad.EArchiveFormat.XML))
+os.remove(txt_file)
+os.remove(xml_file)
 
 #create cell
 cell_name = "cell"
@@ -51,8 +59,30 @@ layout = cell.get_layout_view()
 net = mgr.create_net(layout, net_name)
 assert(net != None)
 
+net_iter = layout.get_net_iter()
+next = net_iter.next()
+assert(next.get_name() == net_name)
+next = net_iter.next()
+assert(next == None)
 
+#create stackup layer
+layer_m = mgr.create_stackup_layer("m", libEcad.ELayerType.ConductingLayer, 0, 10)
+assert(layer_m.thickness == 10)
+layer_v = mgr.create_stackup_layer("v", libEcad.ELayerType.DielectricLayer, 0, 20)
+layer_v.elevation = -10
+assert(layer_v.elevation == -10)
 
+#create layer map
+layer_map_name = "layer_map"
+layer_map = mgr.create_layer_map(database, layer_map_name)
+assert(layer_map.get_database().suuid() == database.suuid())
+
+#create padstack def
+padstack_def_name = "padstack_def"
+padstack_def = mgr.create_padstack_def(database, padstack_def_name)
+
+#create padstack def data
+padstackdef_data = mgr.create_padstack_def_data()
 ###EDataMgr
 
 ###ECell
--- src/ECadPyModule.cpp
+++ src/ECadPyModule.cpp
@@ -1,20 +1,63 @@
 #include "ECadCommon.h"
 
 #ifdef ECAD_BOOST_PYTHON_SUPPORT
+#include "EPadstackDefData.h"
+#include "ENetCollection.h"
+#include "EPadstackDef.h"
 #include "ELayoutView.h"
+#include "ELayerMap.h"
 #include "EDatabase.h"
 #include "EDataMgr.h"
+#include "ELayer.h"
 #include "ECell.h"
 #include "ENet.h"
 #include "EObject.h"
 
-namespace ecad {
-using namespace boost::python;
-}//namespace ecad
-
 namespace {
     using namespace ecad;
     using namespace boost::python;
+
+    /**
+     * @brief Adapter a non-member function that returns a unique_ptr to
+     * a python function object that returns a raw pointer but
+     * explicitly passes ownership to Python.
+     * 
+     * The lambda delegates to the original function, 
+     * and releases() ownership of the instance to Python,
+     * and the call policy indicates that Python will take ownership of the value returned from the lambda.
+     * The mpl::vector describes the call signature to Boost.Python,
+     * allowing it to properly manage function dispatching between the languages.
+     */
+    template <typename T, typename ...Args>
+    object adapt_unique(std::unique_ptr<T> (*fn)(Args...))
+    {
+        return make_function([fn](Args... args){ return fn(args...).release(); },
+                                return_value_policy<manage_new_object>(),
+                                boost::mpl::vector<T*, Args...>());
+    }
+
+    /**
+     * @brief Adapter of member function
+     */
+    template <typename T, typename C, typename ...Args>
+    object adapt_unique(std::unique_ptr<T> (C::*fn)(Args...))
+    {
+        return make_function([fn](C & self, Args... args){ return (self.*fn)(args...).release(); },
+                                return_value_policy<manage_new_object>(),
+                                boost::mpl::vector<T *, C &, Args...>());
+    }
+
+    /**
+     * @brief Adapter of const member function
+     */
+    template <typename T, typename C, typename ...Args>
+    object adapt_unique(std::unique_ptr<T> (C::*fn)(Args...) const)
+    {
+        return make_function([fn](C & self, Args... args){ return (self.*fn)(args...).release(); },
+                                return_value_policy<manage_new_object>(),
+                                boost::mpl::vector<T *, C &, Args...>());
+    }
+
     BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(EDataMgrSaveDatabaseTxt, EDataMgr::SaveDatabase, 2, 3)
     BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(EDataMgrLoadDatabaseTxt, EDataMgr::LoadDatabase, 2, 3)
 
@@ -25,6 +68,13 @@ namespace {
             .value("TXT", EArchiveFormat::TXT)
             .value("XML", EArchiveFormat::XML)
             .value("BIN", EArchiveFormat::BIN)
+        ;
+
+        enum_<ELayerType>("ELayerType")
+            .value("Invalid", ELayerType::Invalid)
+            .value("DielectricLayer", ELayerType::DielectricLayer)
+            .value("ConductingLayer", ELayerType::ConductingLayer)
+            .value("MetalizedSignal", ELayerType::MetalizedSignal)
         ;
 
         //Point
@@ -52,11 +102,54 @@ namespace {
         class_<ENet, bases<EObject, INet> >("ENet", no_init)
         ;
 
+        class_<IIterator<INet>, boost::noncopyable>("INetIter", no_init)
+        ;
+
+        class_<ENetIterator, bases<IIterator<INet> >, boost::noncopyable>("ENetIter", no_init)
+            .def("next", &ENetIterator::Next, return_internal_reference<>())
+            .def("current", &ENetIterator::Current, return_internal_reference<>())
+        ;
+
+        //Layer
+        class_<ILayer, boost::noncopyable>("ILayer", no_init)
+        ;
+
+        class_<ELayer, bases<EObject, ILayer>, boost::noncopyable>("ELayer", no_init)
+        ;
+
+        class_<EStackupLayer, bases<ELayer> >("EStackupLayer", init<std::string, ELayerType>())
+            .add_property("elevation", &EStackupLayer::GetElevation, &EStackupLayer::SetElevation)
+            .add_property("thickness", &EStackupLayer::GetThickness, &EStackupLayer::GetThickness)
+        ;
+
+        //Layer Map
+        class_<ILayerMap, boost::noncopyable>("ILayerMap", no_init)
+        ;
+
+        class_<ELayerMap, bases<EDefinition, ILayerMap> >("ELayerMap", no_init)
+            .def("get_database", &ELayerMap::GetDatabase, return_internal_reference<>())
+        ;
+
+        //Padstack Def
+        class_<IPadstackDef, boost::noncopyable>("IPadstackDef", no_init)
+        ;
+
+        class_<EPadstackDef, bases<EDefinition, IPadstackDef> >("EPadstackDef", init<std::string>())
+        ;
+
+        //Padstack Def Data
+        class_<IPadstackDefData, boost::noncopyable>("IPadstackDefData", no_init)
+        ;
+
+        class_<EPadstackDefData, bases<IPadstackDefData> >("EPadstackDefData", no_init)
+        ;
+
         //LayoutView
         class_<ILayoutView, boost::noncopyable>("ILayoutView", no_init)
         ;
 
         class_<ELayoutView, bases<ILayoutView> >("ELayoutView", no_init)
+            .def("get_net_iter", adapt_unique(&ELayoutView::GetNetIter))
         ;
 
         //Cell
@@ -73,8 +166,6 @@ namespace {
             .def("get_layout_view", &ECircuitCell::GetLayoutView,  return_internal_reference<>()) 
         ;
 
-        // implicitly_convertible<std::shared_ptr<ECell>, std::shared_ptr<ICell> >();
-
         //Database
         class_<IDatabase, std::shared_ptr<IDatabase>, boost::noncopyable>("IDatabase", no_init)
         ;
@@ -82,6 +173,7 @@ namespace {
         class_<EDatabase, bases<IDatabase>, std::shared_ptr<EDatabase> >("EDatabase", init<std::string>())
             .def("set_name", &EDatabase::SetName)
             .def("get_name", &EDatabase::GetName, return_value_policy<copy_const_reference>())
+            .def("suuid", &EDatabase::sUuid)
         ;
 
         implicitly_convertible<std::shared_ptr<EDatabase>, std::shared_ptr<IDatabase> >();
@@ -102,6 +194,10 @@ namespace {
             .def("create_circuit_cell", &EDataMgr::CreateCircuitCell, return_internal_reference<>())
             .def("find_cell_by_name", &EDataMgr::FindCellByName, return_internal_reference<>())
             .def("create_net", &EDataMgr::CreateNet, return_internal_reference<>())
+            .def("create_stackup_layer", adapt_unique(&EDataMgr::CreateStackupLayer))
+            .def("create_layer_map", &EDataMgr::CreateLayerMap, return_internal_reference<>())
+            .def("create_padstack_def", &EDataMgr::CreatePadstackDef, return_internal_reference<>())
+            .def("create_padstack_def_data", adapt_unique(&EDataMgr::CreatePadstackDefData))
         ;
     }
 }
