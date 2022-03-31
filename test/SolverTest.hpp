ace885a5b94b0af04621ae365bcda63174819151
Draaaaaaven <350240702@qq.com>
2022-03-31 18:24:00 +0800

    update

--- test/SolverTest.hpp
+++ test/SolverTest.hpp
@@ -16,7 +16,7 @@ void t_grid_thermal_model_solver_test()
     std::string err;
     std::string ctm = ecad_test::GetTestDataPath() + "/extension/ctm/rhsc_ctm5.tar.gz";
     std::string ctmFolder = ecad_test::GetTestDataPath() + "/extension/ctm/rhsc_ctm5";
-    auto model = io::makeGridThermalModelFromCTMv1File(ctm, 1, &err);
+    auto model = io::makeGridThermalModelFromCTMv1File(ctm, 2, &err);
     BOOST_CHECK(generic::filesystem::PathExists(ctmFolder));
     generic::filesystem::RemoveDir(ctmFolder);
     BOOST_CHECK(model);
