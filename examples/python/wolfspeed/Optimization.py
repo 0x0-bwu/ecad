3d7cf6964b8e949abd8f1e69c3bc995a0ad0fb99
Draaaaaaven <350240702@qq.com>
2024-10-20 01:00:45 +0800

    impl: opimization constrains and bonds

--- examples/python/wolfspeed/Optimization.py
+++ examples/python/wolfspeed/Optimization.py
@@ -35,6 +35,7 @@ def cost_function(parameters) :
     extraction_settings.mesh_settings.max_len = 2
     extraction_settings.mesh_settings.dump_mesh_file = False
     extraction_settings.layer_cut_settings.layer_transition_ratio = 0
+    setup.settings.dump_hotmaps = False
     minT, maxT, chipT = layout.run_thermal_simulation(setup)
 
     print(f'run: {cost_function.counter}, score: {score(chipT)}')
@@ -48,8 +49,30 @@ def main() :
 
     parameters = [-5.23, 8.93, -5.23, 3.86, -5.23, -1.21, 3.71, 8.08, 3.71, 1.33, 3.71, -5.42,
                    5.23, 8.08, 5.23, 1.33, 5.23, -5.42, -3.7, 8.08, -3.7, 1.33, -3.7, -5.42,]
+    
+    bonds = ((-6.95, 0.65), (1.50, 9.50), (-6.95, 0.65), (-2.50, 5.50), (-6.95, 0.65), (-6.50, 1.50),
+             (-2.20, 5.40), (2.75, 9.25), (-2.20, 5.40), (-1.75, 4.75), (-2.20, 5.40), (-6.25, 0.25),
+             (-0.65, 6.95), (1.50, 9.50), (-0.65, 6.95), (-2.50, 5.50), (-0.65, 6.95), (-6.50, 1.50),
+             (-5.40, 2.20), (2.75, 9.25), (-5.40, 2.20), (-1.75, 4.75), (-5.40, 2.20), (-6.25, 0.25),            
+            )
+    
+    conds = ({'type': 'ineq', 'fun': lambda x: x[ 6] - x[ 0] - 5.0},
+             {'type': 'ineq', 'fun': lambda x: x[ 8] - x[ 2] - 5.0},
+             {'type': 'ineq', 'fun': lambda x: x[10] - x[ 4] - 5.0},
+             {'type': 'ineq', 'fun': lambda x: x[ 1] - x[ 3] - 4.0},
+             {'type': 'ineq', 'fun': lambda x: x[ 3] - x[ 5] - 4.0},
+             {'type': 'ineq', 'fun': lambda x: x[ 7] - x[ 9] - 4.5},
+             {'type': 'ineq', 'fun': lambda x: x[ 9] - x[11] - 4.5},
+             {'type': 'ineq', 'fun': lambda x: x[12] - x[18] - 5.0},
+             {'type': 'ineq', 'fun': lambda x: x[14] - x[20] - 5.0},
+             {'type': 'ineq', 'fun': lambda x: x[16] - x[22] - 5.0},
+             {'type': 'ineq', 'fun': lambda x: x[13] - x[15] - 4.0},
+             {'type': 'ineq', 'fun': lambda x: x[15] - x[17] - 4.0},
+             {'type': 'ineq', 'fun': lambda x: x[19] - x[21] - 4.5},
+             {'type': 'ineq', 'fun': lambda x: x[21] - x[23] - 4.5},
+            )
 
-    res = optimize.minimize(cost_function, parameters, method='BFGS', options={'disp': True})
+    res = optimize.minimize(cost_function, parameters, method='SLSQP', bounds=bonds, constraints=conds, options={'disp': True, 'eps': 0.1})
     print(res.x)
     
     mgr.shut_down()
